6dd6fc7f601e213423a4cadac4638d6d646f1f44
André Coêlho <alcbcoelho@gmail.com>
2025-01-02 15:17:35 -0300

    Refactored top-level await occurrences

--- src/spotify.ts
+++ src/spotify.ts
@@ -6,25 +6,32 @@ const accessToken = localStorage.getItem('access_token');
 const refreshToken = localStorage.getItem('refresh_token');
 const expirationTime = localStorage.getItem('expiration_time');
 
-if (
-  accessToken &&
-  refreshToken &&
-  expirationTime &&
-  new Date().getTime() >= parseInt(expirationTime)
-) {
-  await getOrRefreshAccessToken({
-    action: 'refresh',
-    refreshToken
-  });
+async function getAccessToken() {
+  if (
+    accessToken &&
+    refreshToken &&
+    expirationTime &&
+    new Date().getTime() >= parseInt(expirationTime)
+  ) {
+    await getOrRefreshAccessToken({
+      action: 'refresh',
+      refreshToken
+    });
 
-  location.reload();
+    location.reload();
+  }
 }
 
-if (code && !accessToken) {
-  await getOrRefreshAccessToken({
-    action: 'get',
-    code
-  });
+async function refreshAccessToken() {
+  if (code && !accessToken) {
+    await getOrRefreshAccessToken({
+      action: 'get',
+      code
+    });
 
-  location.href = `${location.origin}/viewer`;
+    location.href = `${location.origin}/viewer`;
+  }
 }
+
+getAccessToken();
+refreshAccessToken();
